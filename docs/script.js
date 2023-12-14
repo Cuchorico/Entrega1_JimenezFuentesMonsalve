9751ad465cfc524d4e96f03c9137989458e2953a
Cuchorico <agustn.monsalve@uc.cl>
2023-12-14 16:50:18 -0300

    Commit 217

--- docs/script.js
+++ docs/script.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let image_container_2 = document.getElementsByClassName("image-container-2")[0];
 
     window.onscroll = function () {
-        image_container_2.classList.add("aparecer-image-container-2");
-
         var distancia = window.innerHeight - image_container_2.getBoundingClientRect().top;
 
-        if (distancia >= 100) {
+        if (distancia >= 700) {
             image_container_2.classList.add("aparecer-image-container-2");
+
+        image_container_2.classList.add("aparecer-image-container-2");
         }
 
         console.log(distancia);
