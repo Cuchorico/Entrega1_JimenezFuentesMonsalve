cc778a51507b2379bc0e0f07f88ffff335f6c93a
Cuchorico <agustn.monsalve@uc.cl>
2023-12-15 03:51:46 -0300

    Commit 221

--- docs/script.js
+++ docs/script.js
@@ -49,3 +49,68 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
     );
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-image-container-6");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-container-1");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-container-2");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-texto-9");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-container-5");
+            }
+        }
+    }
+    );
+});
