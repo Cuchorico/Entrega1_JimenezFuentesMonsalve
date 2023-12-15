65382b9870c6810fb292bdd0dea6944cee416892
Cuchorico <agustn.monsalve@uc.cl>
2023-12-15 00:43:05 -0300

    Commit 219

--- docs/script.js
+++ docs/script.js
@@ -4,11 +4,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
     window.onscroll = function () {
         image_container_2.classList.add("aparecer-image-container-2");
 
-        var distancia = window.innerHeight - image_container_2.getBoundingClientRect().top;
+        var revealpoint = window.innerHeight - image_container_2.getBoundingClientRect().top;
 
-        if (distancia >= 94) {
+        if (revealpoint = 700) {
             image_container_2.classList.add("aparecer-image-container-2");
         }
 
-        console.log(distancia);
+        console.log(revealpoint);
     };
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let image_container_3 = document.getElementsByClassName("image-container-3")[0];
+
+    window.onscroll = function () {
+        image_container_2.classList.add("aparecer-image-container-3");
+
+        var revealpoint = window.innerHeight - image_container_3.getBoundingClientRect().top;
+
+        if (revealpoint = 1200) {
+            image_container_2.classList.add("aparecer-image-container-3");
+        }
+
+        console.log(revealpoint);
+    };
+});
