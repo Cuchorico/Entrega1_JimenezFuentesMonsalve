30fab768a1d26ddc0fac09fbc5d0d73059cca09c
Cuchorico <agustn.monsalve@uc.cl>
2023-12-17 21:01:58 -0300

    Commit 259

--- docs/script.js
+++ docs/script.js
@@ -188,6 +188,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
     window.addEventListener('scroll', function() {
         for (let i = 0; i < containers.length; i++) {
             if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-indice-max");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
+                containers[i].classList.add("aparecer-texto-9");
+            }
+        }
+    }
+    );
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let containers = document.getElementsByClassName("image-fading");
+
+    window.addEventListener('scroll', function() {
+        for (let i = 0; i < containers.length; i++) {
+            if (containers[i].getBoundingClientRect().top < window.innerHeight - 94) {
                 containers[i].classList.add("aparecer-texto-10");
             }
         }
